ba883304c05becefba976399f155249cd69e3d1d
DiegoDzC <diego_3141230102@utd.edu.mx>
2024-05-21 22:01:05 -0600

    Programa 9 - Contraseña

---
+++ programa9.py
@@ -0,0 +1,7 @@
+while True:
+    contraseña = input("Ingrese su contraseña: ")
+    if contraseña == "POO123":
+        print("Acceso Autorizado, Bienvenido al programa.")
+        break
+    else:
+        print("Contraseña incorrecta, intentálo de nuevo.")
