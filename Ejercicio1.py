6666e752f528921ccd1d4d4211315299e332cb82
DiegoDzC <diego_3141230102@utd.edu.mx>
2024-05-27 10:50:46 -0600

    Examen Parcial 1

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+print("Ingrese un número: ")
+num = int(input())
+if num > 0:
+    print("+",num," es un Numero positivo")
+elif num < 0:
+    print(num," es un Numero negativo")
+else :
+    print(num," es igual a cero")
