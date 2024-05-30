ef51d0478acb2fbe4710fe4bd58f957236ba023c
DiegoDzC <diego_3141230102@utd.edu.mx>
2024-05-30 08:21:48 -0600

    Programa 18 - Diccionario con descuento

---
+++ programa18.py
@@ -0,0 +1,27 @@
+inventario = {
+    "Manzana" : 31.90,
+    "Platano" : 23.70,
+    "Naranja" : 21.43,
+    "Melon" : 40.99,
+    "Sandia" : 35.90,
+    "Chile" : 20.70,
+    "Tomate" : 19.60,
+    "Cebolla" : 12.10
+}
+
+print("precios sin descuento")
+for key in inventario:
+    value = inventario[key]
+    print(key,": $",value)
+
+for key in inventario:
+    value = inventario[key]
+    descuento = 0.15
+    preciodto = value - (value*descuento)
+    preciofinal = round(preciodto,2)
+    inventario[key] = preciofinal
+
+print("Precios con descuento")
+for key in inventario:
+    value = inventario[key]
+    print(key,": $",value)
