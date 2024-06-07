38e96ac12656fd02f7ac2126bec4540a45dc3ddb
DiegoDzC <diego_3141230102@utd.edu.mx>
2024-06-06 19:40:30 -0600

    Programa 21 - Tupla desempaquetada

---
+++ programa21.py
@@ -0,0 +1,8 @@
+tupla = ("Diego", "18", "Durango", "Mexicano", "Masculino")
+a, b, c, d, e = tupla
+print("-----------Datos Generales-----------")
+print("Nombre: ",a)
+print("Edad: ",b)
+print("Localidad: ",c)
+print("Nacionalidad: ",d)
+print("Sexo: ",e)
